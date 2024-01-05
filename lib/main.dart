5f009fa9156fb2b4aef8fc8be5c2b4ba08155d26
Dongho Gwak <wing.flower.k@gmail.com>
2024-01-05 22:39:38 +0900

    refactor: Router 적용 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:image_search_app_006/data/repository/image_item_repository_impl.dart';
 import 'package:image_search_app_006/di/di_setup.dart';
+import 'package:image_search_app_006/routes.dart';
 import 'package:image_search_app_006/ui/main_screen.dart';
 import 'package:image_search_app_006/ui/main_view_model.dart';
 import 'package:provider/provider.dart';
@@ -15,18 +16,13 @@ class MyApp extends StatelessWidget {
 
     @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
                                                                                                                                 colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ChangeNotifierProvider(
-        create: (_) => MainViewModel(
-          repository: ImageItemRepositoryImpl(),
-        ),
-        child: const MainScreen(),
-      )
     );
   }
 }
